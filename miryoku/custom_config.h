b51ab6e5ba4d19e1f3278cab9386731739a09e67
brs713 <brs713@gmail.com>
2026-04-08 22:04:59 -0500

    populate with 3 definitions
    #define MIRYOKU_ALPHAS_QWERTY
    #define MIRYOKU_TAP_QWERTY
    #define MIRYOKU_CLIPBOARD_WIN

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,9 @@
-// Copyright 2021 Manna Harbour
+// Copyright 2022 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+// QWERTY base and tap layers
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_TAP_QWERTY
+
+// Windows clipboard shortcuts (Ctrl+C/X/V/Z/Y)
+#define MIRYOKU_CLIPBOARD_WIN
